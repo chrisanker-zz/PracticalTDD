1f99680f32dc6cb2fd1ea8cb8b054652d9809a5c
christian-anker-larsen <christian.anker.larsen@maersk.com>
2021-09-29 13:17:59 +0200

    Fixed failing tests

--- test/com/practicetdd/isbntools/StockManagementTests.java
+++ test/com/practicetdd/isbntools/StockManagementTests.java
@@ -2,6 +2,7 @@ package com.practicetdd.isbntools;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import static junit.framework.TestCase.fail;
 
@@ -33,11 +34,38 @@ public class StockManagementTests {
 
     @Test
     public void databaseIsUsedIfDataIsPresent(){
-        fail();
+        ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
+        ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
+
+        when(databaseService.lookup("0395489326")).thenReturn(new Book("0395489326","abc","abc"));
+
+        StockManager stockManager = new StockManager();
+        stockManager.setWebService(webService);
+        stockManager.setDatabaseService(databaseService);
+
+        String isbn = "0395489326";
+        String locatorCode = stockManager.getLocatorCode(isbn);
+
+        verify(databaseService,times(1)).lookup("0395489326");
+        verify(webService, times(0)).lookup(anyString());
     }
 
     @Test
     public void webserviceIsUsedIfDataIsNotPresentInDatabase(){
-        fail();
+        ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
+        ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
+
+        when(databaseService.lookup("0395489326")).thenReturn(null);
+        when(webService.lookup("0395489326")).thenReturn(new Book("0395489326","abc","abc"));
+
+        StockManager stockManager = new StockManager();
+        stockManager.setWebService(webService);
+        stockManager.setDatabaseService(databaseService);
+
+        String isbn = "0395489326";
+        String locatorCode = stockManager.getLocatorCode(isbn);
+
+        verify(databaseService,times(1)).lookup("0395489326");
+        verify(webService, times(1)).lookup("0395489326");
     }
 }
